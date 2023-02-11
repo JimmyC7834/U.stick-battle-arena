fe0bdf960e295b5a9a680b49212abf28a45d7cbb
irontigeroutreach <118071231+irontigeroutreach@users.noreply.github.com>
2023-02-11 11:48:03 -0800

    working player ui for player one

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -2,38 +2,64 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
+
 
 public class UIManager : MonoBehaviour
 {
     // Start is called before the first frame update
+
+
+ 
     public TextMeshProUGUI playerOneScoreLabel;
+    public TextMeshProUGUI playerOneLivesLeftLabel;
     public GameObject playerOneDurability;
     public GameObject playerOneHealth;
     public GameObject playerOneInventory;
+
+
     public int playerOneScoreNum;
-    public int playerOneDurabilityScore;
-    public int playerOneHealthScore;
+    public int playerOneLivesLeftNum;
+    public float playerOneDurabilityScore = 0.2f;
+    public float playerOneHealthScore;
     public string playerOneInventoryType;
 
-    public GameObject playerTwoScore;
-    public GameObject playerTwoDurability;
-    public GameObject playerTwoHealth;
-    public GameObject playerTwoInventory;
-    public int playerTwoScoreNum;
-    public int playerTwoDurabilityScore;
-    public int playerTwoHealthScore;
-    public string playerTwoInventoryType;
+    //public GameObject playerTwoScore;
+    //public GameObject playerTwoDurability;
+    //public GameObject playerTwoHealth;
+    //public GameObject playerTwoInventory;
+    //public int playerTwoScoreNum;
+    //public int playerTwoDurabilityScore;
+    //public int playerTwoHealthScore;
+    //public string playerTwoInventoryType;
 
 
 
     private void Update()
     {
-        if (Input.GetKeyDown("space"))
+        if (Input.GetKeyDown("z"))
         {
             changePlayerOneScore(1);
             Debug.Log("click");
         }
-        
+
+        if (Input.GetKeyDown("x"))
+        {
+            changePlayerOneDurability(0.4f);
+            Debug.Log("click");
+        }
+
+        if (Input.GetKeyDown("c"))
+        {
+            changePlayerOneHealth(0.4f);
+            Debug.Log("click");
+        }
+
+        if (Input.GetKeyDown("v"))
+        {
+            changePlayerOneLivesLeft(-1);
+        }
+
     }
 
 
@@ -41,43 +67,58 @@ public class UIManager : MonoBehaviour
     {
         playerOneScoreNum = playerOneScoreNum + changeBy;
         playerOneScoreLabel.text = playerOneScoreNum.ToString();
+        //Image buttonImage = newTextureButton.gameObject.GetComponent<Image>();
     }
 
-    public void changePlayerTwoScore(int changeBy)
+    public void changePlayerOneDurability(float changeBy)
     {
-        playerTwoScoreNum = playerTwoScoreNum + changeBy;
+        playerOneDurabilityScore = playerOneDurabilityScore + changeBy;
+        playerOneDurability.GetComponent<Image>().fillAmount = playerOneDurabilityScore;
+        //change player
     }
 
-    public void changePlayerOneHealth(int changeBy)
+    public void changePlayerOneHealth(float changeBy)
     {
         playerOneHealthScore = playerOneHealthScore + changeBy;
-
-        //change playerOneHealthBar
-
-    }
-
-
-    public void changePlayerTwoHealth(int changeBy)
-    {
-        playerTwoHealthScore = playerTwoHealthScore + changeBy;
-
-        //change playerOneHealthBar
-
-    }
-
-    public void changePlayerOneDurability(int changeBy)
-    {
-        playerOneDurabilityScore = playerOneDurabilityScore + changeBy; 
-
+        playerOneHealth.GetComponent<Image>().fillAmount = playerOneHealthScore;
         //change player
     }
 
-    public void changePlayerTwoDurability(int changeBy)
+    public void changePlayerOneLivesLeft(int changeBy)
     {
-        playerTwoDurabilityScore = playerTwoDurabilityScore + changeBy;
-
-        //change player
+        playerOneLivesLeftNum = playerOneLivesLeftNum + changeBy;
+        playerOneLivesLeftLabel.text = playerOneLivesLeftNum.ToString();
     }
+
+
+
+
+
+
+
+
+
+
+    //public void changePlayerTwoScore(int changeBy)
+    //{
+    //    playerTwoScoreNum = playerTwoScoreNum + changeBy;
+    //}
+
+
+    //public void changePlayerTwoHealth(int changeBy)
+    //{
+    //    playerTwoHealthScore = playerTwoHealthScore + changeBy;
+
+    //    //change playerOneHealthBar
+
+    //}
+
+    //public void changePlayerTwoDurability(int changeBy)
+    //{
+    //    playerTwoDurabilityScore = playerTwoDurabilityScore + changeBy;
+
+    //    //change player
+    //}
 
 
     public void changePlayerOneWeaponType(string weaponType)
@@ -87,10 +128,10 @@ public class UIManager : MonoBehaviour
         //change player one weapon image
     }
 
-    public void changePlayerTwoWeaponType(string weaponType)
-    {
-        playerTwoInventoryType = weaponType;
-    }
+    //public void changePlayerTwoWeaponType(string weaponType)
+    //{
+    //    playerTwoInventoryType = weaponType;
+    //}
 
 
 
