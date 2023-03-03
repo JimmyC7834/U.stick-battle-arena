75e7e92306476889347f7c4e1590f8d698971085
JimmyCheong <c7834@uw.edu>
2023-03-03 01:36:20 -0800

    Added scrolling background for side panels

---
+++ Assets/Scripts/UI/UI_CutOutMask.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
+
+namespace Game.UI
+{
+    public class UI_CutOutMask : RawImage
+    {
+        public override Material materialForRendering
+        {
+            get
+            {
+                Material mat = new Material(base.materialForRendering);
+                mat.SetInt("_StencilComp", (int) CompareFunction.NotEqual);
+                return mat;
+            }
+        }
+    }
+}
