dfe49cea653f96e6a1531261ac54fe5d9f1b86b6
JimmyCheong <c7834@uw.edu>
2023-03-04 02:19:36 -0800

    Added logic for target score mode

---
+++ Assets/Scripts/SOs/HighScoreGameModeLogicSO.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+    /**
+     * Game mode logic for battle royal mode.
+     * The game end with there's only one player's score >= target score
+     */
+    [CreateAssetMenu]
+    public class HighScoreGameModeLogicSO : GameModeLogicSO
+    {
+        [SerializeField] private GameplayService _gameplayService;
+        [SerializeField] private int _targetScore;
+        
+        protected override void HookEvents()
+        {
+            _gameplayService.PlayerManager.OnScoreChange += CheckForWinner;
+        }
+        
+        protected override void UnHookEvents()
+        {
+            _gameplayService.PlayerManager.OnScoreChange -= CheckForWinner;
+        }
+
+        private void CheckForWinner(PlayerID id)
+        {
+            if (_gameplayService.PlayerManager.GetScore(id) >= _targetScore)
+                InvokeGameEndedEvent(id);
+        }
+    }
+}
