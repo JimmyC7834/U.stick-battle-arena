f6fd5ebc56ead3c394a9289f9f3d858816d6868a
JimmyCheong <c7834@uw.edu>
2023-03-03 20:06:54 -0800

    Added input reader for Menu input Added pause menu

---
+++ Assets/Scripts/UI/UI_PauseMenu.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game.UI
+{
+    public class UI_PauseMenu : MonoBehaviour
+    {
+        [SerializeField] private GameService _gameService;
+        [SerializeField] private GameInputReader _gameInputReader;
+        [SerializeField] private Button _continueButton;
+        [SerializeField] private Button _returnToMainMenuButton;
+
+        private void Awake()
+        {
+            _gameInputReader.escEvent += HandleEsc;
+            
+            _continueButton.onClick.AddListener(ClosePauseMenu);
+            _returnToMainMenuButton.onClick.AddListener(ReturnToMainMenu);
+
+            gameObject.SetActive(false);
+        }
+
+        private void ReturnToMainMenu()
+        {
+            _gameService.SceneManager.LoadScene(SceneID.MainMenu);
+        }
+
+        private void HandleEsc()
+        {
+            if (!isActiveAndEnabled)
+            {
+                OpenPauseMenu();
+                return;
+            }
+            
+            ClosePauseMenu();
+        }
+
+        private void OpenPauseMenu()
+        {
+            Time.timeScale = 0;
+            gameObject.SetActive(true);
+        }
+        
+        private void ClosePauseMenu()
+        {
+            Time.timeScale = 1;
+            gameObject.SetActive(false);
+        }
+    }
+}
