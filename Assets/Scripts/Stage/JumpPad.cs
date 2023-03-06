e471142e0f19f5476c0daef09a8e65213a5f61f9
JimmyCheong <c7834@uw.edu>
2023-03-05 22:08:10 -0800

    Added bouncing visual effects to jump pad Tuned the jump pad prefab

--- Assets/Scripts/Stage/JumpPad.cs
+++ Assets/Scripts/Stage/JumpPad.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using System;
 
@@ -14,7 +12,24 @@ namespace Game
     {
 
         // Strength of the jump pad, force added in the upwards direction
-        [SerializeField] private float jumpStrength = 500f;
+        [SerializeField] private float _jumpStrength = 500f;
+        [SerializeField] private Transform _visualTransform;
+        [SerializeField] private float _visualShakeDistance;
+        [SerializeField] private float _visualShakeReturnSpeed;
+        private Vector3 _origin;
+
+        private void Awake()
+        {
+            _origin = _visualTransform.position;
+        }
+
+        private void Update()
+        {
+            _visualTransform.position = Vector2.Lerp(
+                _visualTransform.position, 
+                _origin, 
+                _visualShakeReturnSpeed * Time.deltaTime);
+        }
 
         // Detect anything with rigidBody component
         private void OnTriggerEnter2D(Collider2D col)
@@ -23,7 +38,8 @@ namespace Game
             // Cancel out vertical velocity
             target.velocity = new Vector3(target.velocity.x, Math.Abs(target.velocity.y), 0);
             // Add upwards force
-            target.AddForce(new Vector3(0, jumpStrength, 0));
+            target.AddForce(new Vector3(0, _jumpStrength, 0));
+            _visualTransform.position += _visualShakeDistance * Vector3.down;
         }
     }
 }
