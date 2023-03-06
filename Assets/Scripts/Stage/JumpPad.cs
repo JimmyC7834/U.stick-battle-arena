79d099092a080c57a6b5d77ff9cdb8bb9192d46a
Fettuchoony <106009124+Fettuchoony@users.noreply.github.com>
2023-03-05 21:08:47 -0800

    Completed Jump Pad
    Pretty basic prefab, cancels vertical velocity and adds a developer chosen amount of upwards force.

--- Assets/Scripts/Stage/JumpPad.cs
+++ Assets/Scripts/Stage/JumpPad.cs
@@ -1,16 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 
 namespace Game
 {
+    /*
+    * Basic JumpPad script that cancels y-axis velocity and applies y-axis force to rigid bodies upon collision
+    */
+    [RequireComponent(typeof(Collider2D), typeof(Rigidbody2D))]
     public class JumpPad : MonoBehaviour
     {
+
+        // Strength of the jump pad, force added in the upwards direction
+        [SerializeField] private float jumpStrength = 500f;
+
+        // Detect anything with rigidBody component
         private void OnTriggerEnter2D(Collider2D col)
         {
             Rigidbody2D target = col.gameObject.GetComponent<Rigidbody2D>();
-            target.AddForce(new Vector3(0, 100, 0));
+            // Cancel out vertical velocity
+            target.velocity = new Vector3(target.velocity.x, Math.Abs(target.velocity.y), 0);
+            // Add upwards force
+            target.AddForce(new Vector3(0, jumpStrength, 0));
         }
     }
 }
