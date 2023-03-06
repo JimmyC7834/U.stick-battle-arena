05330ad2430497bbd83231889cd0774179371f71
Fettuchoony <106009124+Fettuchoony@users.noreply.github.com>
2023-03-05 16:49:50 -0800

    Added KillZone
    Added the killzone and modified player stat class to have Damage Info constructor without damage origin parameter for stage related damage.

---
+++ Assets/Scripts/Player/KillZone.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Player {
+
+	/*
+	 * Apply to GameObject with collider set to isTrigger
+	 * Upon collision, deducts 9999 health from the player.
+	 */
+	public class KillZone: MonoBehaviour {
+		
+    	private void OnTriggerEnter2D(Collider2D col)
+        {
+	        // Grab player who collided
+	        PlayerStat target = col.gameObject.GetComponent<PlayerStat>();
+	        // Deduct their HP
+			target.DeductHealth(new DamageInfo(target.ID, target.ID, 9999));
+        }
+	}
+}
+
