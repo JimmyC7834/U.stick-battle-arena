f12c0cb4d0e8a4d0fc8305b06ee16f972cfe42f5
Phoenix Yi <gaoywin1@uw.edu>
2023-03-06 20:34:25 -0800

    combat weapons finalized

--- Assets/Scripts/Weapon/MeleeWeapon.cs
+++ Assets/Scripts/Weapon/MeleeWeapon.cs
@@ -2,7 +2,9 @@
 
 using System.Collections;
 using Game.Player;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 #endregion
 
@@ -11,13 +13,16 @@ namespace Game
     public abstract class MeleeWeapon : UsableItem
     {
         public int Damage => _damage;
+        public int Score => _score;
         public bool IsAvailable = true;
-        [SerializeField] private float CooldownDuration;
+        [SerializeField] private float _cooldownDuration;
         [SerializeField] private int _damage;
-        [SerializeField] private Vector2 _knockbackDirection;
-        [SerializeField] private float _knockback;
+        [SerializeField] private int _score;
+        [SerializeField] private float _thrust;
         [SerializeField] private float _rayCastRadius;
         [SerializeField] private Transform _hitBoxOrigin;
+        
+        private Vector2 _knockbackDirection;
 
         protected void Attack(PlayerID attacker)
         {
@@ -25,36 +30,58 @@ namespace Game
             _service.AudioManager.PlayAudio(_audioOnUse);
             ReduceDurability(1);
             
-            Collider2D col = Physics2D.OverlapCircle(_hitBoxOrigin.position, _rayCastRadius);
-            if (col == null) return;
-            
-            // check if hit a player
-            PlayerStat target = col.GetComponent<PlayerStat>();
-            if (target != null && target.ID != attacker)
+            Collider2D[] cols = Physics2D.OverlapCircleAll(_hitBoxOrigin.position, _rayCastRadius);
+            if (cols == null) return;
+
+            foreach (Collider2D col in cols)
             {
-                // Deduct health of the hit other player
-                target.DeductHealth(
-                    new DamageInfo(
+                // check if hit a player
+                PlayerStat target = col.GetComponent<PlayerStat>();
+                if (target != null && target.ID != attacker)
+                {
+                    // Deduct health of the hit other player
+                    DamageInfo damageInfo = new DamageInfo(
                         attacker,
                         target.ID,
                         _damage,
-                        this));
-                Vector2 tempKnockback = new Vector2(_knockbackDirection.x * transform.parent.localScale.x * -1,
-                    _knockbackDirection.y);
-                Rigidbody2D playerBody = target.GetComponent<Rigidbody2D>();
-                playerBody.velocity += tempKnockback * _knockback;
-
+                        this);
+                    target.DeductHealth(damageInfo);
+                
+                    // Add Score to the attacker
+                    _service.PlayerManager.IncreaseScore(attacker, _score);
+                    KnockBack(damageInfo);
+                }
             }
             
-            _service.AudioManager.PlayAudio(_audioOnUse);
-            ReduceDurability(1);
             StartCoroutine((StartCooldown()));
         }
-        
-        public IEnumerator StartCooldown()
+
+        private void KnockBack(DamageInfo damageInfo)
+        {
+            
+            PlayerMovement target = _service.PlayerManager.
+                GetPlayerStat(damageInfo.Target).GetComponent<PlayerMovement>();
+            PlayerMovement attacker = _service.PlayerManager.
+                GetPlayerStat(damageInfo.Dealer).GetComponent<PlayerMovement>();
+
+            Vector2 force;
+            
+            if (!attacker.IsFacingLeft)
+            {
+                force = Vector2.right * _thrust;
+            }
+            else
+            {
+                force = Vector2.left * _thrust;
+            }
+            
+            target.RB.velocity += force * _thrust;
+        }
+
+        private IEnumerator StartCooldown()
         {
             IsAvailable = false;
-            yield return new WaitForSeconds(CooldownDuration);
+            yield return new WaitForSeconds(_cooldownDuration);
             IsAvailable = true;
         }
     }
