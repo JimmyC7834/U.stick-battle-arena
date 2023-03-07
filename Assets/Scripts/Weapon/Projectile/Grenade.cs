09f397bd8fab492e61a47f27e8c6e417a241791b
Phoenix Yi <gaoywin1@uw.edu>
2023-03-07 00:22:59 -0800

    Weapons finished

---
+++ Assets/Scripts/Weapon/Projectile/Grenade.cs
@@ -0,0 +1,64 @@
+#region
+
+using System;
+using Game.Player;
+using UnityEngine;
+
+#endregion
+
+namespace Game
+{
+    public class Grenade : Projectile
+    {
+        [SerializeField] private float _splashRadius;
+        [SerializeField] private float _explodeTime;
+
+        private float _currTime;
+        
+        private void Start()
+        {
+            _currTime = _explodeTime;
+            OnHitStage += HandleHitStage;
+            OnHitProjectile += _ => HandleHitStage();
+            OnHitPlayer += (_) => Explode();
+        }
+
+        private void Update()
+        {
+            if (_currTime > 0)
+            {
+                _currTime -= Time.deltaTime;
+            }
+            else
+            {
+                Explode();
+                _currTime = _explodeTime;
+            }
+        }
+
+        private void HandleHitStage()
+        {
+            _hit = false;
+        }
+
+        private void Explode()
+        {
+            Collider2D[] hits = Physics2D.OverlapCircleAll(
+                transform.position, 
+                _splashRadius);
+            foreach (Collider2D hit in hits)
+            {
+                PlayerStat player = hit.GetComponent<PlayerStat>();
+                if (player == null) continue;
+                
+                DamageInfo damageInfo = CreateDamageInfo(player.ID);
+                // Deduct health of the player
+                if (damageInfo.Dealer != damageInfo.Target)
+                    _service.PlayerManager.IncreaseScore(damageInfo.Dealer, _score);
+                _service.PlayerManager.
+                    GetPlayerStat(damageInfo.Target).DeductHealth(damageInfo);
+            }
+            ReturnToPool();
+        }
+    }
+}
---
+++ Assets/Scripts/Weapon/Projectile/Rocket.cs
@@ -0,0 +1,40 @@
+#region
+
+using Game.Player;
+using UnityEngine;
+
+#endregion
+
+namespace Game
+{
+    public class Rocket : Projectile
+    {
+        [SerializeField] private float _splashRadius;
+        
+        private void Start()
+        {
+            OnHitPlayer += (_) => Explode();
+            OnHitStage += Explode;
+        }
+
+        private void Explode()
+        {
+            Collider2D[] hits = Physics2D.OverlapCircleAll(
+                transform.position, 
+                _splashRadius);
+            foreach (Collider2D hit in hits)
+            {
+                PlayerStat player = hit.GetComponent<PlayerStat>();
+                if (player == null) continue;
+                
+                DamageInfo damageInfo = CreateDamageInfo(player.ID);
+                // Deduct health of the player
+                if (damageInfo.Dealer != damageInfo.Target)
+                    _service.PlayerManager.IncreaseScore(damageInfo.Dealer, _score);
+                _service.PlayerManager.
+                    GetPlayerStat(damageInfo.Target).DeductHealth(damageInfo);
+            }
+            ReturnToPool();
+        }
+    }
+}
